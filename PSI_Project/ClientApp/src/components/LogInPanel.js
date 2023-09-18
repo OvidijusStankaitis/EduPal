f0f665b9151fbfbae67140003589ba5cecd13852
OvidijusStankaitis <ovidijus.stankaitis@gmail.com>
2023-09-19 02:43:25 +0300

    Added buttons that navigate to Admin/User panels (placeholders) after the placeholder login prompt

---
+++ PSI_Project/ClientApp/src/components/LogInPanel.js
@@ -0,0 +1,38 @@
+﻿import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './LogInPanel.css';
+
+export const LogInPanel = () => {
+    const [showDialog, setShowDialog] = useState(false);
+    const [targetRoute, setTargetRoute] = useState('');
+    const navigate = useNavigate();
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault();
+        navigate(targetRoute);
+    };
+
+    const showLoginDialog = (route) => {
+        setTargetRoute(route);
+        setShowDialog(true);
+    };
+
+    return (
+        <div className={showDialog ? "container blurred" : "container"}>
+            <span className="buttons">
+                <a onClick={() => showLoginDialog('/user')} className="btn-flip user" data-back="LOG IN" data-front="USER"></a>
+                <a onClick={() => showLoginDialog('/admin')} className="btn-flip admin" data-back="LOG IN" data-front="ADMIN"></a>
+            </span>
+            {showDialog &&
+                <div className="dialog">
+                    <form onSubmit={handleLoginSubmit}>
+                        <input type="text" placeholder="Username" required />
+                        <input type="password" placeholder="Password" required />
+                        <button type="submit">Log In</button>
+                        <button type="button" onClick={() => setShowDialog(false)}>Close</button>
+                    </form>
+                </div>
+            }
+        </div>
+    );
+};
---
+++ PSI_Project/ClientApp/src/components/UserPanel.js
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+export const UserPanel = () => {
+    return <div>User Panel</div>;
+}
