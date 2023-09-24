c658b919703a4ebf1063d9df854713c3e14613fe
Denis Olechnovic <116972879+Myshk-IN@users.noreply.github.com>
2023-09-24 14:48:58 +0300

    Subject and Topic classes are added

---
+++ PSI_Project/Topic.cs
@@ -0,0 +1,31 @@
+﻿namespace PSI_Project;
+
+public class Topic
+{
+    public Topic(string topicName, string topicDescription)
+    {
+        Name = topicName;
+        Description = topicDescription;
+        Rating = 0;
+        Comments = new List<string>();
+    }
+    public short Rating { get; set; } 
+    public string Name {get; set;}
+    public string Description {get; set;}
+    
+    public List<String> Comments { get; set;}
+    
+    public void DownloadFileFromURL(string URL) //SHOULD BE USED IN Task class instead?
+    {
+        using (var client = new HttpClient())
+        {
+            using (var stream = client.GetStreamAsync("http://www.mif.vu.lt/katedros/se/Sandai/13_14/4s_PSI_II_LT.pdf"))//URL must be inserted here
+            {
+                using (var fileStream = new FileStream("C:\\Users\\denol\\OneDrive\\Desktop\\localfile.pdf", FileMode.OpenOrCreate))
+                {
+                    stream.Result.CopyTo(fileStream);
+                }
+            }
+        }
+    }
+}
