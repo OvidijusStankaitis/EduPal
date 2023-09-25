8e9fd73b5fd6519a8df9515bb2dffd531cb25088
OvidijusStankaitis <ovidijus.stankaitis@gmail.com>
2023-09-25 05:16:27 +0300

    Finished the ui chenges, created the app name, enabled pdf file upload and view

--- PSI_Project/ClientApp/src/components/LogInPanel.js
+++ PSI_Project/ClientApp/src/components/LogInPanel.js
@@ -5,10 +5,21 @@ import './LogInPanel.css';
 export const LogInPanel = () => {
     const [showDialog, setShowDialog] = useState(false);
     const [targetRoute, setTargetRoute] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
     const navigate = useNavigate();
 
     const handleLoginSubmit = (e) => {
         e.preventDefault();
+        navigate(targetRoute);
+    };
+
+    const handleRegisterSubmit = (e) => {
+        e.preventDefault();
+        if (password !== repeatPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
         navigate(targetRoute);
     };
 
@@ -19,18 +30,36 @@ export const LogInPanel = () => {
 
     return (
         <div className={showDialog ? "container blurred" : "container"}>
+            <h1 className="edu">EduPal</h1>
             <span className="buttons">
-                <a onClick={() => showLoginDialog('/user')} className="btn-flip user" data-back="LOG IN" data-front="USER"></a>
-                <a onClick={() => showLoginDialog('/admin')} className="btn-flip admin" data-back="LOG IN" data-front="ADMIN"></a>
+                <button onClick={() => showLoginDialog('/user')}>USER</button>
+                <button onClick={() => showLoginDialog('/admin')}>ADMIN</button>
             </span>
             {showDialog &&
                 <div className="dialog">
-                    <form onSubmit={handleLoginSubmit}>
-                        <input type="text" placeholder="Username" required />
-                        <input type="password" placeholder="Password" required />
-                        <button type="submit">Log In</button>
-                        <button type="button" onClick={() => setShowDialog(false)}>Close</button>
-                    </form>
+                    <div className="dialog-header">Login/Register</div>
+                    <div className="dialog-content">
+                        <div className="form-section">
+                            <form onSubmit={handleLoginSubmit}>
+                                <input type="email" placeholder="Email" required />
+                                <input type="password" placeholder="Password" required />
+                                <button type="submit">Log In</button>
+                            </form>
+                        </div>
+                        <div className="form-section">
+                            <form onSubmit={handleRegisterSubmit}>
+                                <input type="text" placeholder="Name" required />
+                                <input type="text" placeholder="Surname" required />
+                                <input type="email" placeholder="Email" required />
+                                <input type="password" placeholder="Password" required value={password} onChange={e => setPassword(e.target.value)} />
+                                <input type="password" placeholder="Repeat Password" required value={repeatPassword} onChange={e => setRepeatPassword(e.target.value)} />
+                                <button type="submit">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="dialog-footer">
+                        <button type="button" onClick={() => setShowDialog(false)}>Cancel</button>
+                    </div>
                 </div>
             }
         </div>
