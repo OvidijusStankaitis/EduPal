7c466841f5f3d794e7ae607426782644c9647aba
Vytautas Mielkus <mielkus.vytautas@gmail.com>
2023-09-28 02:35:43 +0300

    Incorporated IComparable and IEquatable Interfaces with Subject.cs class. Currently it uses IComparablle to always sort the Subject items list in alphabetical order, or after any operation such as CRUD operations. Also created CRUD operation methods for topic and generalised them with generic methods in BaseHandler.cs class.

--- PSI_Project/Models/BaseHandler.cs
+++ PSI_Project/Models/BaseHandler.cs
@@ -7,13 +7,6 @@ public abstract class BaseHandler<T> where T : BaseEntity
     // This abstract property ensures that derived classes provide the correct file path.
     protected abstract string DbFilePath { get; }
 
-    public T CreateItem(T item)
-    {
-        Items.Add(item);
-        WriteItemToDB(item);
-        return item;
-    }
-
     protected void WriteItemToDB(T item)
     {
         using (var streamWriter = new StreamWriter(DbFilePath, true))
@@ -41,4 +34,38 @@ public abstract class BaseHandler<T> where T : BaseEntity
 
     // Derived classes must implement how a string from the database is converted to an item.
     protected abstract T StringToItem(string dbString);
+
+    public virtual T CreateItem(T item)
+    {
+        Items.Add(item);
+        WriteItemToDB(item);
+        AfterOperation();
+        return item;
+    }
+
+    public virtual bool RemoveItem(T item)
+    {
+        bool removed = Items.Remove(item);
+        if (removed) AfterOperation();
+        return removed;
+    }
+
+    public virtual T ModifyItem(T oldItem, T newItem)
+    {
+        int index = Items.IndexOf(oldItem);
+        if (index != -1)
+        {
+            Items[index] = newItem;
+            AfterOperation();
+        }
+
+        return newItem;
+    }
+
+    // This method is executed after any operation. By default, it does nothing.
+    // Derived classes can override it to add specific behavior, like sorting. 
+    // Only overriden in SubjectHandler class, not TopicHandler class.
+    protected virtual void AfterOperation()
+    {
+    }
 }
--- PSI_Project/Models/Topic/TopicHandler.cs
+++ PSI_Project/Models/Topic/TopicHandler.cs
@@ -6,7 +6,7 @@ public class TopicHandler : BaseHandler<Topic>
     {
         ReadAllItemsFromDB();
     }
-    
+
     protected override string DbFilePath => "..//DB//TopicInformation.txt";
 
     protected override string ItemToDbString(Topic item)
