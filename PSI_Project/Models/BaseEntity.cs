a00cb246dca838e23cd41833e4b3c47680c1dde1
zhemepatis <rink.gab@gmail.com>
2023-09-29 20:17:25 +0300

    added HelperFunctions and DatabaseOperations folders, started restructuring project

--- PSI_Project/Models/BaseEntity.cs
+++ PSI_Project/Models/BaseEntity.cs
@@ -3,7 +3,7 @@
 public class BaseEntity
 {
     public string Name {get; set;}
-    public string Description {get; set;}
+    public string? Description {get; set;}
 
 
     public BaseEntity(string name, string description)
--- PSI_Project/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSI_Project;
-
-public class User
-{
-    private string Id { get; set; }
-    private string Username { get; set;  }
-    private string FirstName { get; set; }
-    private string SecondName { get; set; }
-}
