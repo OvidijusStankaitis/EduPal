15b94b0dc182118be9ecf1bd33da735f211fede0
OvidijusStankaitis <ovidijus.stankaitis@gmail.com>
2023-10-02 19:40:26 +0300

    Integrated enpoints to view new subjects / topics

--- PSI_Project/Controllers/TopicController.cs
+++ PSI_Project/Controllers/TopicController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PSI_Project.Controllers;
 
@@ -8,29 +9,35 @@ public class TopicController : ControllerBase
 {
     private readonly TopicHandler _topicHandler = new TopicHandler();
 
-    [HttpGet]
-    public IActionResult ListTopics()
+    [HttpGet("list/{subjectName}")]
+    public IActionResult ListTopics(string subjectName)
     {
-        return Ok(_topicHandler.ItemList);
+        List<Topic> subjectTopics = new List<Topic>();
+        foreach (var topic in _topicHandler.ItemList)
+        {
+            if (topic.SubjectName == subjectName)
+            {
+                subjectTopics.Add(topic);
+            }
+        }
+        return Ok(subjectTopics); 
     }
     
     [HttpPost("upload")]
-    public IActionResult UploadTopic(string topicName, string topicDescription, string subjectName)
+    public IActionResult UploadTopic([FromBody] JsonElement request)
     {
-        _topicHandler.CreateItem(new Topic(topicName, topicDescription, subjectName));
-        return Ok(_topicHandler.ItemList);
-    }
-    
-    [HttpPost("{topicOldName}/modify")]
-    public IActionResult ModifyTopic(string topicOldName,string topicName, string topicDescription, string subjectName)
-    {
-        Topic? oldTopic = _topicHandler.CheckItemInList(topicOldName);
-        if (oldTopic != null)
+        if (request.TryGetProperty("topicName", out var topicNameProperty) && 
+            request.TryGetProperty("topicDescription", out var topicDescriptionProperty) &&
+            request.TryGetProperty("subjectName", out var subjectNameProperty))
         {
-            _topicHandler.ModifyItem(oldTopic, new Topic(topicName, topicDescription, subjectName));
+            string topicName = topicNameProperty.GetString();
+            string topicDescription = topicDescriptionProperty.GetString() ?? " ";
+            string subjectName = subjectNameProperty.GetString();
+
+            _topicHandler.CreateItem(new Topic(topicName, topicDescription, subjectName));
             return Ok(_topicHandler.ItemList);
         }
-        return NotFound();
+        return BadRequest("Invalid request body");
     }
     
     [HttpDelete("{topicName}/delete")] //should "delete/" be here?
