77bf89141230898533baa5eadb0ae0c92467958e
Vytautas Mielkus <mielkus.vytautas@gmail.com>
2023-10-14 17:19:44 +0300

    Created Goals and SubjectGoal.cs classes for study goals implementation

---
+++ PSI_Project/Models/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace PSI_Project.Models
+{
+    public class Goal
+    {
+        public string Id { get; init; }
+        public string UserId { get; set; }
+        public List<SubjectGoal> SubjectGoals { get; set; }
+
+        public Goal(string userId)
+        {
+            Id = Guid.NewGuid().ToString();
+            UserId = userId;
+            SubjectGoals = new List<SubjectGoal>();
+        }
+    }
+}
---
+++ PSI_Project/Models/SubjectGoal.cs
@@ -0,0 +1,18 @@
+﻿namespace PSI_Project.Models
+{
+    public class SubjectGoal
+    {
+        public string SubjectId { get; set; }
+        public double TargetHours { get; set; }
+        
+        // Actual time studied for this subject
+        public double ActualHoursStudied { get; set; }
+
+        public SubjectGoal(string subjectId, double targetHours)
+        {
+            SubjectId = subjectId;
+            TargetHours = targetHours;
+            ActualHoursStudied = 0; // 0 hours studied on initialization
+        }
+    }
+}
