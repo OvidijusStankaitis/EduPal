48efa5531f4abfe99d21d9abee0f1770e1e84ec0
Denis Olechnovic <116972879+Myshk-IN@users.noreply.github.com>
2023-10-15 13:58:53 +0300

    Added CommentController.csn (for that added an addition method in CommentRepository.cs)

---
+++ PSI_Project/Controllers/CommentController.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PSI_Project.Models;
+using PSI_Project.Repositories;
+
+namespace PSI_Project.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CommentController : ControllerBase
+{
+    private readonly CommentRepository _commentRepository = new CommentRepository();
+    
+    [HttpGet("getOne/{commentId}")]
+    public IActionResult GetComment(string commentId)
+    {
+        Comment? comment = _commentRepository.GetItemById(commentId);
+        return comment == null
+            ? NotFound(new { error = "Comment not found." })
+            : Ok(comment);
+    }
+    
+    [HttpGet("get/{conspectusId}")]
+    public IActionResult GetAllCommentsFromConspectus(string conspectusId)
+    {
+        List<Comment> comment = _commentRepository.GetAllCommentsOfConspectus(conspectusId);
+        return Ok(comment);
+    }
+    
+    [HttpPost("upload")]
+    public IActionResult UploadComment([FromBody] JsonElement request)
+    {
+        Comment? comment = _commentRepository.CreateComment(request);
+        return comment == null
+            ? BadRequest("Invalid request body")
+            : Ok(comment);
+    }
+    
+    [HttpDelete("{commentId}/delete")]
+    public IActionResult RemoveTopic(string commentId)
+    {
+        return _commentRepository.RemoveItemById(commentId) 
+            ? Ok("Topic has been successfully deleted")
+            : BadRequest("An error occured while deleting the topic");
+    }
+}
--- PSI_Project/Repositories/CommentRepository.cs
+++ PSI_Project/Repositories/CommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using PSI_Project.Models;
+﻿using System.Text.Json;
+using PSI_Project.Models;
 
 namespace PSI_Project.Repositories;
 
@@ -9,6 +10,24 @@ public class CommentRepository : BaseRepository<Comment>
     public List<Comment> GetAllCommentsOfConspectus(string conspectusId)
     {
         return Items.Where(comment => comment.ConspectusId.Equals(conspectusId)).ToList();
+    }
+    
+    public Comment? CreateComment(JsonElement request) //needs to be tested
+    {
+        if (request.TryGetProperty("commentText", out var commentTextProperty) &&
+            request.TryGetProperty("conspectusId", out var conspectusIdProperty))
+        {
+            string? commentText = commentTextProperty.GetString();
+            string? conspectusId = conspectusIdProperty.GetString();
+            
+            if (conspectusId != null && commentText != null)
+            {
+                Comment newComment = new Comment(conspectusId:conspectusId, commentText:commentText);
+                if(InsertItem(newComment));
+                return newComment;
+            }
+        }
+        return null;
     }
     
     protected override string ItemToDbString(Comment item)
