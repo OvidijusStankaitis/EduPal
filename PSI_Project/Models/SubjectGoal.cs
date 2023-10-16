7545b0e27a9b7efada18cd0a531d8b204149ebd9
Vytautas Mielkus <mielkus.vytautas@gmail.com>
2023-10-16 21:35:33 +0300

    Added struct to SubjectGoal.cs class

--- PSI_Project/Models/SubjectGoal.cs
+++ PSI_Project/Models/SubjectGoal.cs
@@ -4,15 +4,34 @@
     {
         public string SubjectId { get; set; }
         public double TargetHours { get; set; }
-        
-        // Actual time studied for this subject
         public double ActualHoursStudied { get; set; }
-
+        
         public SubjectGoal(string subjectId, double targetHours)
         {
             SubjectId = subjectId;
             TargetHours = targetHours;
             ActualHoursStudied = 0; // 0 hours studied on initialization
+        }
+        
+        public struct StudyDuration // 1. Struct used, not implemented yet TODO: view goals implementation which uses this struct
+        {
+            public int Hours { get; set; }
+            public int Minutes { get; set; }
+
+            public static StudyDuration ToReadableTime(double totalHours)
+            {
+                int totalMinutes = (int)(totalHours * 60);
+                return new StudyDuration
+                {
+                    Hours = totalMinutes / 60,
+                    Minutes = totalMinutes % 60
+                };
+            }
+
+            public override string ToString()
+            {
+                return $"{Hours} hours and {Minutes} minutes";
+            }
         }
     }
 }
