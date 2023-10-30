a94cc2aca99b8bac6871b8cb2df7ec9144965010
OvidijusStankaitis <ovidijus.stankaitis@gmail.com>
2023-10-30 22:13:51 +0200

    Modified notes frontend

--- PSI_Project/ClientApp/src/components/Note.js
+++ PSI_Project/ClientApp/src/components/Note.js
@@ -4,6 +4,8 @@ import "./Note.css";
 export const Note = ({ show, onClose, topicId }) => {
     const [noteName, setNoteName] = useState("");
     const [noteContent, setNoteContent] = useState("");
+    const [showDialog, setShowDialog] = useState(false);
+    const [savedNotes, setSavedNotes] = useState([]);
 
     const handleExport = async () => {
         const response = await fetch("https://localhost:7283/Note/create-pdf", {
@@ -25,11 +27,34 @@ export const Note = ({ show, onClose, topicId }) => {
         }
     };
 
+    const handleShowDialog = () => {
+        setShowDialog(true);
+    }
+
+    const handleCloseDialog = () => {
+        setShowDialog(false);
+    };
+
+    const handleSave = () => {
+        setSavedNotes(prevNotes => [...prevNotes, { name: noteName, content: noteContent }]);
+        setNoteName("");  // Clear current note name
+        setNoteContent("");  // Clear current note content
+    };
+
+    const handleSelectNote = (selectedNote) => {
+        setNoteName(selectedNote.name);
+        setNoteContent(selectedNote.content);
+        setShowDialog(false);  // Close the dialog after selecting a note
+    };
+
     if (!show) return null;
 
     return (
         <div className="note">
-            <h1>New Note</h1>
+            <div className="header2">
+                <h1>New Note</h1>
+                <button className="modify-note" onClick={handleShowDialog}>Saved Notes</button>
+            </div>
             <input
                 className="note-name"
                 placeholder="Name"
@@ -43,9 +68,28 @@ export const Note = ({ show, onClose, topicId }) => {
                 onChange={e => setNoteContent(e.target.value)}
             />
             <div className="button-group1">
+                <button className="modify-note" onClick={handleSave}>Save</button>
                 <button className="modify-note" onClick={handleExport}>Export</button>
                 <button className="modify-note" onClick={onClose}>Cancel</button>
             </div>
+
+            {showDialog &&
+                <div className="dialog">
+                    <ul className="notes-list">
+                        {savedNotes.length > 0 ? (
+                            savedNotes.map((note, index) => (
+                                <li key={index} onClick={() => handleSelectNote(note)}>
+                                    {note.name}
+                                </li>
+                            ))
+                        ) : (
+                            <li className="notes-empty">No saved notes</li>
+                        )}
+                    </ul>
+                    <div className="dialog-footer">
+                        <button className="close-note" onClick={handleCloseDialog}>Close</button>
+                    </div>
+                </div>}
         </div>
     );
 }
