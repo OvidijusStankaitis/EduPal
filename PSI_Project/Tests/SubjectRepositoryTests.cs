9d9bf3877dc32194a246314cd4a67c3aa1d6afc1
Denis Olechnovic <116972879+Myshk-IN@users.noreply.github.com>
2023-10-30 23:08:39 +0200

    Added (first) unit tests for Subject, Topic, and User repositories

---
+++ PSI_Project/Tests/SubjectRepositoryTests.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PSI_Project.Data;
+using PSI_Project.Models;
+using PSI_Project.Repositories;
+using Xunit;
+
+namespace PSI_Project.Tests;
+
+public class SubjectRepositoryTests
+{
+    private readonly SubjectRepository _subjectRepository;
+    private readonly DbContextOptions<EduPalDatabaseContext> _options;
+    private readonly EduPalDatabaseContext _context;
+    
+    public SubjectRepositoryTests()
+    {
+        _options = GetInMemoryDatabaseOptions("TestDB"); 
+        _context = new EduPalDatabaseContext(_options); //should it be in using? 
+        _subjectRepository = new SubjectRepository(_context);
+        
+    }
+    
+    private DbContextOptions<EduPalDatabaseContext> GetInMemoryDatabaseOptions(string dbName)
+    {
+        return new DbContextOptionsBuilder<EduPalDatabaseContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+    }
+
+    [Fact]
+    public void GetSubjectsList_DBIsEmpty_ReturnsEmpty()
+    {
+        // Arrange
+        
+        // Act
+        var result = _subjectRepository.GetSubjectsList();
+
+        // Assert
+        Assert.Empty(result);
+    }
+    
+    [Fact]
+    public void GetSubjectsList_DBContainsOneElement_ReturnsAListOfOneElement()
+    {
+        // Arrange
+        var subject = new Subject("testSubject");
+        _subjectRepository.Add(subject);
+        _context.SaveChanges();
+        
+        // Act
+        var result = _subjectRepository.GetSubjectsList();
+        _subjectRepository.RemoveSubject(subject.Id); // removing subject so DB would be empty for the GetSubjectsList_ListIsEmpty_ReturnsNull() method
+        
+        // Assert
+        Assert.Single(result);
+        Assert.Collection(result, subject1 => Assert.Contains(subject.Name, subject1.Name));
+    }
+    
+    [Fact]
+    public void GetSubjectsList_DBContainsTwoElement_ReturnsAListOfOneElement()
+    {
+        // Arrange
+        var subject1 = new Subject("testSubject1");
+        _subjectRepository.Add(subject1);
+        _context.SaveChanges();
+       
+        var subject2 = new Subject("testSubject2");
+        _subjectRepository.Add(subject2);
+        _context.SaveChanges();
+        
+        // Act
+        var result = _subjectRepository.GetSubjectsList();
+        _subjectRepository.RemoveSubject(subject1.Id); // removing subject so DB would be empty for the GetSubjectsList_ListIsEmpty_ReturnsNull() method
+        _subjectRepository.RemoveSubject(subject2.Id); // removing subject so DB would be empty for the GetSubjectsList_ListIsEmpty_ReturnsNull() method
+        
+        // Assert
+        Assert.NotEmpty(result);
+        Assert.Collection(result, 
+            subjectOfList1 => Assert.Contains(subject1.Name, subjectOfList1.Name),
+            subjectOfList2 => Assert.Contains(subject2.Name, subjectOfList2.Name));
+    }
+    
+    [Fact]
+    public void RemoveSubject_UserExists_ReturnsTrue()
+    {
+        // Arrange
+        var subject = new Subject("removeSubjectTestName");
+        _subjectRepository.Add(subject);
+        _context.SaveChanges();
+        
+        // Act
+        var result = _subjectRepository.RemoveSubject(subject.Id);
+
+        // Assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void RemoveSubject_UserDoesNotExist_ReturnsFalse()
+    {
+        // Arrange
+        var subject = new Subject("nonexistentTestRemoveSubject");
+        
+        // Act
+        var result = _subjectRepository.RemoveSubject(subject.Id);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    
+}
--- PSI_Project/Tests/UserRepositoryTests.cs
+++ PSI_Project/Tests/UserRepositoryTests.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using PSI_Project;
+﻿using Microsoft.EntityFrameworkCore;
 using PSI_Project.Data;
-using PSI_Project.Models;
 using PSI_Project.Repositories;
 using Xunit;
 namespace PSI_Project.Tests;
 
 public class UserRepositoryTests
 {
+    private readonly UserRepository _userRepository;
+    private readonly DbContextOptions<EduPalDatabaseContext> _options;
+    private readonly EduPalDatabaseContext _context;
+
+    public UserRepositoryTests()
+    {
+        _options = GetInMemoryDatabaseOptions("TestDB"); 
+        _context = new EduPalDatabaseContext(_options); //should it be in using? 
+        _userRepository = new UserRepository(_context);
+        
+        //adding one test element for a test DB 
+        var user = new User("test@test.test", "testPassword", "testName", "testSurname");
+        _context.Users.Add(user);
+        _context.SaveChanges();
+    }
+    
     private DbContextOptions<EduPalDatabaseContext> GetInMemoryDatabaseOptions(string dbName)
     {
         return new DbContextOptionsBuilder<EduPalDatabaseContext>()
@@ -21,16 +33,61 @@ public class UserRepositoryTests
     public void CheckUserRegister_UserDoesNotExist_ReturnsTrue()
     {
         // Arrange
-        var options = GetInMemoryDatabaseOptions("EduPal");
-        using var context = new EduPalDatabaseContext(options);
-        var userRepository = new UserRepository(context);
-
-        var user = new User("test@example.com", "testPassword", "testName", "testSurname");
+        var user = new User("nonexistentTest@test.test", "nonexistentTestPassword", "nonexistentTestName", "nonexistentTestSurname");
 
         // Act
-        var result = userRepository.CheckUserRegister(user);
-
+        var result = _userRepository.CheckUserRegister(user);
+        var users = _context.Users.ToList();
+        foreach (var u in users)
+        {
+            Console.WriteLine(u);
+        }
         // Assert
         Assert.True(result);
+    }
+    
+    [Fact]
+    public void CheckUserRegister_UserExists_ReturnsFalse()
+    {
+        // Arrange
+        
+        var userExisting = new User("test@test.test", "testPassword", "testName", "testSurname");
+        // Act
+        var result = _userRepository.CheckUserRegister(userExisting);
+
+        // Assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void GetUserByEmail_UserExists_ReturnsUser()
+    {
+        // Arrange
+        var user = new User("test1@test.test", "testPassword", "testName", "testSurname");
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        // Act
+        var result = _userRepository.GetUserByEmail("test1@test.test");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("test1@test.test", result.Email);
+        Assert.Equal("testPassword", result.Password);
+        Assert.Equal("testName", result.Name);
+        Assert.Equal("testSurname", result.Surname);
+        
+    }
+
+    [Fact]
+    public void GetUserByEmail_UserDoesNotExist_ReturnsNull()
+    {
+        // Arrange
+        
+        // Act
+        var result = _userRepository.GetUserByEmail("nonexistent@test.test");
+
+        // Assert
+        Assert.Null(result);
     }
 }
