6253bedff80407fc71cfa0b9c29c9e65fbd490ac
zhemepatis <rink.gab@gmail.com>
2023-11-05 11:09:19 +0200

    removed unused constructors

--- PSI_Project/Exceptions/ObjectNotFoundException.cs
+++ PSI_Project/Exceptions/ObjectNotFoundException.cs
@@ -2,15 +2,7 @@
 
 public class ObjectNotFoundException : Exception
 {
-    public ObjectNotFoundException()
-    {
-    }
-
     public ObjectNotFoundException(string message) : base(message)
-    {
-    }
-
-    public ObjectNotFoundException(string message, Exception inner) : base(message, inner)
     {
     }
 }
