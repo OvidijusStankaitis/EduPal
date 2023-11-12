84786595fee0488383da47a497c98d6ffce6d6b8
zhemepatis <rink.gab@gmail.com>
2023-11-12 21:20:30 +0200

    added new migration for notes

--- PSI_Project/Migrations/20231112190841_NotesTable.Designer.cs
+++ PSI_Project/Migrations/20231112190841_NotesTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PSI_Project.Data;
@@ -11,9 +12,11 @@ using PSI_Project.Data;
 namespace PSI_Project.Migrations
 {
     [DbContext(typeof(EduPalDatabaseContext))]
-    partial class EduPalDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20231112190841_NotesTable")]
+    partial class NotesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
